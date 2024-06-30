13a76a122fe423848416a1569246938fc70c4290
adhsandb05 <adhikarisandeeb@gmail.com>
2024-06-30 17:23:53 -0400

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Ensure pip is available
-python -m ensurepip --upgrade
+python3.9 -m ensurepip --upgrade
 
 # Install virtualenv if not already installed
-python -m pip install --upgrade virtualenv
+python3.9 -m pip install --upgrade virtualenv
 
 # Create a virtual environment
-python -m virtualenv venv
+python3.9 -m virtualenv venv
 
 # Activate the virtual environment
 source venv/bin/activate
 
+# Upgrade pip
+python3.9 -m pip install --upgrade pip
+
 # Install the requirements
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Run collectstatic
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 
 # Run migrations
-python manage.py migrate
+python3.9 manage.py migrate
