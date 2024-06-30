5b4fdae533726b40ebb7a9b5722b369b7a2b4c8f
adhsandb05 <adhikarisandeeb@gmail.com>
2024-06-30 16:49:10 -0400

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,2 @@
-#!/bin/bash
-
-# Create a virtual environment
-python -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Upgrade pip
-pip install --upgrade pip
-
-# Install the requirements
 pip install -r requirements.txt
-
-# Run collectstatic
-python manage.py collectstatic --noinput
-
-# Run migrations
-python manage.py migrate
+python3.9 manage.py collectstatic
